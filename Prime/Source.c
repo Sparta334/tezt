dca16481966b5e668a79f17d7344f13f8919b639
Spartan334 <ianmina0322@gmail.com>
2024-02-26 20:56:45 +0800

    黑11q

---
+++ Prime/Source.c
@@ -0,0 +1,67 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int Prime(int Target) {
+
+
+	
+	for (int i = 2 ; i <= Target-1 ; i++) {
+
+		if (Target % i == 0 ){
+			return 0 ;
+		}
+
+
+	}
+
+	return 1;
+
+	
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(  ) {
+
+	
+	int answer = Prime(5);
+	
+
+
+
+	
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
